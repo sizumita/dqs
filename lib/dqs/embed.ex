2d4154be3eddee8242e483232bd74e6b5a78a234
Sumito Izumita <sumito@izumita.com>
2021-03-10 22:21:23 +0900

    change embed

--- lib/dqs/embed.ex
+++ lib/dqs/embed.ex
@@ -15,6 +15,7 @@ defmodule Dqs.Embed do
            info.original_message_id
          })/
        )
+    |> put_field("タグ", if(question.tag == [], do: "なし", else: question.tag |> Enum.join(", ")))
     |> put_color(color)
   end
 end
