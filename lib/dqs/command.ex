56a4383147e7f45997daca172d0eb1ad834074a2
Sumito Izumita <sumito@izumita.com>
2021-03-10 17:05:30 +0900

    create create question system

--- lib/dqs/command.ex
+++ lib/dqs/command.ex
@@ -1,16 +1,15 @@
 defmodule Dqs.Command do
-  alias Dqs.Repo
+  @prefix System.get_env("PREFIX")
 
-  def on_message(%{content: "!create"} = msg) do
-    Repo.insert(
-      %Dqs.Question{
-        issuer_id: msg.author.id,
-        name: "test question",
-        content: "テストがしたい",
-        status: "open",
-        channel_id: msg.channel_id,
-        tag: ["test"]
-      }
-    ) |> IO.inspect
+  def handle(%{content: @prefix <> "create " <> _name} = msg) do
+    Dqs.Command.Create.handle(msg)
+  end
+
+  def handle(_msg) do
+    :noop
+  end
+
+  defp create_message(channel_id, message) do
+    Nostrum.Api.create_message(channel_id, message)
   end
 end
