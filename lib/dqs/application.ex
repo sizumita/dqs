35674dda405331528d2f4574eca2c926853b626f
sizumita <sumito@izumita.com>
2021-05-16 23:44:39 +0900

    Add delay when close command

--- lib/dqs/application.ex
+++ lib/dqs/application.ex
@@ -9,12 +9,16 @@ defmodule Dqs.Application do
     children = [
       Dqs.Consumer,
       Dqs.Repo,
-      {Cachex, name: :ratelimit}
+#      {Cachex, name: :ratelimit},
+#      {Cachex, name: :delay_close}
+# https://elixirforum.com/t/how-to-run-more-then-one-same-worker-cachex-inside-supervision/14911
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Dqs.Supervisor]
     Supervisor.start_link(children, opts)
+    Cachex.start_link(:ratelimit)
+    Cachex.start_link(:delay_close)
   end
 end
